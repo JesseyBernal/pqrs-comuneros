ddc721ee021f3351df840a1751adf18fff4aac44
Jessey Jack Bernal Diaz <jesseybernal@gmail.com>
2024-09-11 16:00:25 -0500

    paginacion y cantidad de estados

--- src/database/query.sql
+++ src/database/query.sql
@@ -29,18 +29,21 @@ CREATE TABLE IF NOT EXISTS estados (
 	descripcion_estado TEXT,
 	PRIMARY KEY (id_estado)
 );
-CREATE TABLE IF NOT EXISTS pqrsds (
-	id_pqrsd INT,
-	id_usuario INT,
-	id_categoria INT,
-	id_estado INT,
-	fecha VARCHAR(255),
-	asunto TEXT,
-	PRIMARY KEY (id_pqrsd),
-	FOREIGN KEY (id_categoria) REFERENCES categorias(id_categoria),
-	FOREIGN KEY (id_estado) REFERENCES estados(id_estado),
-	FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario)
+
+CREATE TABLE IF NOT EXISTS estados_locales (
+  id_estado_local INT AUTO_INCREMENT,
+  nombre_estado VARCHAR(255),
+  descripcion_estado VARCHAR(255),
+  PRIMARY KEY (id_estado_local)
 );
+
+CREATE TABLE IF NOT EXISTS administradores (
+  id_administrador INT AUTO_INCREMENT,
+  nombre_administrador VARCHAR(255),
+  descripcion_administrador VARCHAR(255),
+  PRIMARY KEY (id_administrador)
+);
+
 CREATE TABLE IF NOT EXISTS usuarios (
 	id_usuario INT,
 	id_empresa INT,
@@ -52,8 +55,33 @@ CREATE TABLE IF NOT EXISTS usuarios (
 	FOREIGN KEY (id_cargo) REFERENCES cargos(id_cargo),
 	FOREIGN KEY (id_empresa) REFERENCES empresas(id_empresa)
 );
-COMMIT;
 
+CREATE TABLE IF NOT EXISTS locales (
+  id_local INT,
+  id_usuario INT,
+  id_estado_local INT,
+  nombre_local VARCHAR(255),
+  descripcion_local VARCHAR(255),
+  PRIMARY KEY (id_local),
+  FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario),
+  FOREIGN KEY (id_estado_local) REFERENCES estados_locales(id_estado_local)
+);
+
+CREATE TABLE IF NOT EXISTS pqrsds (
+	id_pqrsd INT,
+	id_local INT,
+	id_categoria INT,
+	id_estado INT,
+	id_administrador INT,
+	fecha VARCHAR(255),
+	asunto TEXT,
+	PRIMARY KEY (id_pqrsd),
+	FOREIGN KEY (id_categoria) REFERENCES categorias(id_categoria),
+	FOREIGN KEY (id_estado) REFERENCES estados(id_estado),
+	FOREIGN KEY (id_local) REFERENCES locales(id_local),
+	FOREIGN KEY (id_administrador) REFERENCES administradores(id_administrador)
+);
+COMMIT;
 
 SELECT * FROM usuarios;
 
@@ -61,7 +89,7 @@ INSERT INTO cargos VALUES (1,'cliente','dirige un local');
 INSERT INTO cargos VALUES (2,'administrador','administra una zona');
 INSERT INTO cargos VALUES (3,'contador','realiza la contabilidad');
 INSERT INTO cargos VALUES (4,'secretario','apoyo a los directivos u otros');
-INSERT INTO cargos VALUES (8,'Gatisimo','Gatooooo');
+
 
 INSERT INTO categorias VALUES (1,'Peticion','Solicitud verbal o escrita que presenta una persona natural o juridica');
 INSERT INTO categorias VALUES (2,'Quejas','Inconformidad por parte de una persona, ya sea de carácter administrativa o por conductas no deseables de los colaboradores');
@@ -79,16 +107,27 @@ INSERT INTO estados VALUES (2,'Pendiente','Aun no se responde el pqrsd');
 INSERT INTO estados VALUES (3,'En espera','Esperando respuesta de un ente externo');
 INSERT INTO estados VALUES (4,'Resuelto','Resuelto el pqrsd');
 
-INSERT INTO pqrsds VALUES (54,36148159,2,2,'2024-08-22','Queja de precio');
-INSERT INTO pqrsds VALUES (987,38231313,4,3,'2024-08-22','Sugiere horario de cierre');
-INSERT INTO pqrsds VALUES (2134,26427749,1,1,'2024-08-22','Solicita activacion de electricidad');
-INSERT INTO pqrsds VALUES (32547,1075223106,3,3,'2024-08-22','Reclama falta de aseo');
+INSERT INTO estados_locales VALUES (1,'Activo','Local en uso');
+INSERT INTO estados_locales VALUES (2,'Inactivo','Local inactivo');
+
+INSERT INTO administradores VALUES (1,'Juan Carlos Segura','Gerente Administrativo');
+INSERT INTO administradores VALUES (2,'Lorena','Recepcion');
 
 INSERT INTO usuarios VALUES (26427749,1,1,'REBECA MOSQUERA MEDINA',3208146410,'rebeca@gmail.com');
 INSERT INTO usuarios VALUES (36148159,1,1,'MIRYAM MADRIGAL DE SANTOS',3281464100,'miryam@gmail.com');
 INSERT INTO usuarios VALUES (38231313,1,2,'FLOR ALBA ROJAS DE CANDIL',3264100814,'alba@gmail.com');
-INSERT INTO usuarios VALUES (42132212,1,1,'REBECA MOSQUERAS ',3208146410,'rebeca@gmail.com');
 INSERT INTO usuarios VALUES (1075223106,1,1,'EVA CAROLINA CARVAJAL ROJAS ',3214641008,'eva@gmail.com');
+
+INSERT INTO locales VALUES (1001,26427749, 1, 'Local ROJAS ', 'Venta de rojas');
+INSERT INTO locales VALUES (1002,36148159, 2, 'Local SANTOS ', 'Venta de santos');
+INSERT INTO locales VALUES (1003,1075223106, 1, 'Local CARVAJAL ', 'Venta de carvajal');
+INSERT INTO locales VALUES (1004,38231313, 2, 'Local CANDIL ', 'Venta de candil');
+
+INSERT INTO pqrsds VALUES (54, 1001, 2, 2, 1, '2024-08-22','Queja de precio');
+INSERT INTO pqrsds VALUES (987, 1002, 4, 3, 2,'2024-08-22','Sugiere horario de cierre');
+INSERT INTO pqrsds VALUES (2134, 1003, 1, 1, 1,'2024-08-22','Solicita activacion de electricidad');
+INSERT INTO pqrsds VALUES (32547, 1004, 3, 3, 2,'2024-08-22','Reclama falta de aseo');
+
 
 UPDATE pqrsds SET id_categoria = 1 WHERE id_pqrsd = 54;
 
@@ -106,3 +145,11 @@ INNER JOIN estados ON pqrsds.id_estado = estados.id_estado;
 SELECT nombre_empresa FROM pqrsds 
 INNER JOIN usuarios ON pqrsds.id_usuario = usuarios.id_usuario
 INNER JOIN empresas ON usuarios.id_empresa = empresas.id_empresa;
+
+
+SELECT id_pqrsd, pqrsds.id_local, nombre_administrador, nombre_usuario, nombre_categoria, nombre_estado,  fecha, asunto FROM pqrsds 
+INNER JOIN locales ON pqrsds.id_local = locales.id_local
+INNER JOIN usuarios ON locales.id_usuario = usuarios.id_usuario 
+INNER JOIN administradores ON pqrsds.id_administrador = administradores.id_administrador
+INNER JOIN categorias ON pqrsds.id_categoria = categorias.id_categoria 
+INNER JOIN estados ON pqrsds.id_estado = estados.id_estado;
--- src/routes/personas.routes.js
+++ src/routes/personas.routes.js
@@ -5,27 +5,50 @@ const router = Router();
 
 router.get('/list', async(req,res) =>{
     try{
-        const [result] = await pool.query('SELECT id_pqrsd, nombre_usuario, nombre_categoria, nombre_estado,  fecha, asunto FROM pqrsds INNER JOIN usuarios ON pqrsds.id_usuario = usuarios.id_usuario INNER JOIN categorias ON pqrsds.id_categoria = categorias.id_categoria INNER JOIN estados ON pqrsds.id_estado = estados.id_estado;');
-        res.render('personas/list', {personas: result})
+        const [result] = await pool.query('SELECT id_pqrsd, pqrsds.id_local AS local, nombre_administrador, nombre_usuario, nombre_categoria, nombre_estado,  fecha, asunto FROM pqrsds INNER JOIN locales ON pqrsds.id_local = locales.id_local INNER JOIN usuarios ON locales.id_usuario = usuarios.id_usuario INNER JOIN administradores ON pqrsds.id_administrador = administradores.id_administrador INNER JOIN categorias ON pqrsds.id_categoria = categorias.id_categoria INNER JOIN estados ON pqrsds.id_estado = estados.id_estado ORDER BY fecha DESC;');
+        const [contarPendiente] = await pool.query('SELECT COUNT(*) AS pendiente FROM pqrsds WHERE id_estado = 2;')
+        const [contarEspera] = await pool.query('SELECT COUNT(*) AS espera FROM pqrsds WHERE id_estado = 3;')
+        const cPendiente = contarPendiente[0];
+        const cEspera = contarEspera[0];
+        res.render('personas/list', {personas: result, contarPendiente: cPendiente, contarEspera: cEspera})
 
     }catch(err){
         res.status(500).json({message:err.message});
     }
 })
+  
+
+router.get('/list/page', async(req, res) => {
+        const [users] = await pool.query('SELECT id_pqrsd, pqrsds.id_local AS local, nombre_administrador, nombre_usuario, nombre_categoria, nombre_estado,  fecha, asunto FROM pqrsds INNER JOIN locales ON pqrsds.id_local = locales.id_local INNER JOIN usuarios ON locales.id_usuario = usuarios.id_usuario INNER JOIN administradores ON pqrsds.id_administrador = administradores.id_administrador INNER JOIN categorias ON pqrsds.id_categoria = categorias.id_categoria INNER JOIN estados ON pqrsds.id_estado = estados.id_estado ORDER BY fecha DESC;')
+        const [contarPendiente] = await pool.query('SELECT COUNT(*) AS pendiente FROM pqrsds WHERE id_estado = 2;')
+        const [contarEspera] = await pool.query('SELECT COUNT(*) AS espera FROM pqrsds WHERE id_estado = 3;')
+        const cPendiente = contarPendiente[0];
+        const cEspera = contarEspera[0];
+        const page = req.query.page
+        const limit = req.query.limit
+
+        const starIndex = (page - 1) * limit
+        const endIndex = page * limit
+        
+        const resultUsers = users.slice(starIndex, endIndex)
+        res.render('personas/list', {personas: resultUsers, contarPendiente: cPendiente, contarEspera: cEspera})
+   
+})
 
 router.get('/add', async(req,res) =>{
-    const [categoria] = await pool.query('SELECT id_categoria, nombre_categoria FROM categorias');
-    const [estado] = await pool.query('SELECT id_estado, nombre_estado FROM estados');
+    const [categoria] = await pool.query('SELECT id_categoria, nombre_categoria FROM categorias;');
+    const [estado] = await pool.query('SELECT id_estado, nombre_estado FROM estados;');
+    const [administrador] = await pool.query('SELECT id_administrador, nombre_administrador FROM administradores;');
     //await pool.query('SELECT id_categoria, nombre_categoria FROM categorias;')
-    res.render('personas/add', {seleccionCategoria: categoria, seleccionEstado: estado});
+    res.render('personas/add', {seleccionCategoria: categoria, seleccionEstado: estado, seleccionAdministrador: administrador});
     
 })
 
 router.post('/add', async(req,res) => {
     try{
-        const {id_pqrsd, id_usuario, id_categoria, id_estado, fecha, asunto} = req.body;
+        const {id_pqrsd, id_local, id_categoria, id_estado,  asunto, fecha, id_administrador} = req.body;
         const newPqrsd = {
-            id_pqrsd, id_usuario, id_categoria, id_estado, fecha, asunto
+            id_pqrsd, id_local, id_categoria, id_estado,  asunto, fecha, id_administrador
         }        
 
         await pool.query('INSERT INTO pqrsds SET ?', [newPqrsd]);
@@ -40,16 +63,12 @@ router.post('/add', async(req,res) => {
 router.get('/edit/:id' , async(req,res) =>{
     try {
         const {id} = req.params;
-        const [categoria] = await pool.query('SELECT id_categoria, nombre_categoria FROM categorias');
-        const [estado] = await pool.query('SELECT id_estado, nombre_estado FROM estados');
-        const [persona] = await pool.query('SELECT * FROM pqrsds INNER JOIN categorias ON pqrsds.id_categoria = categorias.id_categoria INNER JOIN estados ON pqrsds.id_estado = estados.id_estado WHERE id_pqrsd = ?', [id]);
+        const [categoria] = await pool.query('SELECT id_categoria, nombre_categoria FROM categorias;');
+        const [estado] = await pool.query('SELECT id_estado, nombre_estado FROM estados;');
+        const [administrador] = await pool.query('SELECT id_administrador, nombre_administrador FROM administradores;');
+        const [persona] = await pool.query('SELECT * FROM pqrsds INNER JOIN locales ON pqrsds.id_local = locales.id_local INNER JOIN categorias ON pqrsds.id_categoria = categorias.id_categoria INNER JOIN estados ON pqrsds.id_estado = estados.id_estado INNER JOIN administradores ON pqrsds.id_administrador = administradores.id_administrador WHERE id_pqrsd = ?;', [id]);
         const personaEdit = persona[0];
-        res.render('personas/edit', {seleccionCategoria: categoria, seleccionEstado: estado, persona: personaEdit});
-        
-    //await pool.query('SELECT id_categoria, nombre_categoria FROM categorias;')
-    //res.render('personas/add', {});
-
-
+        res.render('personas/edit', {seleccionCategoria: categoria, seleccionEstado: estado, persona: personaEdit, seleccionAdministrador: administrador});
     } catch (err) {
         res.status(500).json({message:err.message});
     }
@@ -57,10 +76,10 @@ router.get('/edit/:id' , async(req,res) =>{
 
 router.post('/edit/:id' , async(req,res) => {
     try {
-        const {id_pqrsd, id_usuario, id_categoria, id_estado, fecha, asunto} = req.body;
         const {id} = req.params;
-        const editPersona = {id_pqrsd, id_usuario, id_categoria, id_estado, fecha, asunto};
-        await pool.query('UPDATE pqrsds SET ? WHERE id_pqrsd = ?', [editPersona, id]);
+        const {id_pqrsd, id_local, id_categoria, id_estado, asunto, fecha, id_administrador} = req.body;        
+        const editPersona = {id_pqrsd, id_local, id_categoria, id_estado, asunto, fecha, id_administrador};
+        await pool.query('UPDATE pqrsds SET ? WHERE id_pqrsd = ?;', [editPersona, id]);
         res.redirect('/list');
     } catch (err) {
         res.status(500).json({message:err.message});
@@ -70,7 +89,7 @@ router.post('/edit/:id' , async(req,res) => {
 router.get('/delete/:id', async(req, res) =>{
     try {
         const {id} = req.params;
-        await pool.query('DELETE FROM pqrsds WHERE id_pqrsd = ?', [id]);
+        await pool.query('DELETE FROM pqrsds WHERE id_pqrsd = ?;', [id]);
         res.redirect('/list');
     } catch (err) {
         res.status(500).json({message:err.message});
